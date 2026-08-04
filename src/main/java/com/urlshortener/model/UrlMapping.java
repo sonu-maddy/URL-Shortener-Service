9130f33576e93407e66bb3415c43ce985dcd8e11
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2026-08-04 18:46:57 +0530

    added

--- src/main/java/com/urlshortener/model/UrlMapping.java
+++ src/main/java/com/urlshortener/model/UrlMapping.java
@@ -39,7 +39,7 @@ public class UrlMapping {
     private LocalDateTime expiryDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = true)
     private User owner;
 
     @PrePersist
